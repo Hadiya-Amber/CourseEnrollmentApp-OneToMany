ef5a03f92936421f526a4cc4a30c50f673b3c0fd
Hadiya-Amber <154611305+Hadiya-Amber@users.noreply.github.com>
2025-08-19 18:59:25 +0530

    Add files via upload

---
+++ RoleBasedAuth/Controllers/TokenController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Tokens.Models;
+
+namespace Tokens.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly DeptDbContext _con;
+        private readonly IConfiguration _configuration;
+
+        public TokenController(DeptDbContext con, IConfiguration configuration)
+        {
+            _con = con;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(User userData)
+        {
+            if (userData != null && !string.IsNullOrEmpty(userData.Email) && !string.IsNullOrEmpty(userData.Password))
+            {
+                var user = await GetUser(userData.Email, userData.Password);
+
+                if (user != null && user.UserId != 0) // check valid user
+                {
+                    var token = GenerateToken(user);
+                    return Ok(new { token });
+                }
+                else
+                {
+                    return BadRequest("Invalid credentials");
+                }
+            }
+            else
+            {
+                return BadRequest("Invalid request data");
+            }
+        }
+
+        private async Task<User?> GetUser(string email, string password)
+        {
+            return await _con.Users.FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+        }
+
+        // ✅ Token generation logic directly inside controller
+        private string GenerateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.Name ?? string.Empty),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email ?? string.Empty),
+                new Claim(ClaimTypes.Role,user.Role)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["TokenKey"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(2),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
