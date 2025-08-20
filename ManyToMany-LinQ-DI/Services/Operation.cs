451b60c6b27fabfe20fcc808d52417fa9d28ccbf
Hadiya-Amber <154611305+Hadiya-Amber@users.noreply.github.com>
2025-08-20 10:51:51 +0530

    Add files via upload

---
+++ ManyToMany-LinQ-DI/Services/Operation.cs
@@ -0,0 +1,13 @@
+﻿namespace ManyToMany.Services
+{
+    public interface IOperation { string Id { get; } }
+    public interface IOperationTransient : IOperation { }
+    public interface IOperationScoped : IOperation { }
+    public interface IOperationSingleton : IOperation { }
+
+    public class Operation :
+        IOperationTransient, IOperationScoped, IOperationSingleton
+    {
+        public string Id { get; } = Guid.NewGuid().ToString("N");
+    }
+}
